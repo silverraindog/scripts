919e363a1664dc0c32e9ecffd70834ea5331f95f
Angus Robinson <angus@fairhaven.za.net>
2025-02-01 17:09:11 +0200

    added youtube and fixed resync

--- rsync.sh
+++ rsync.sh
@@ -1,29 +1,23 @@
 #!/usr/bin/env bash
 set -x
-start="21:00"
-end="06:30"
 #user is the user to login to the server
-user="root"
+user="sargus"
 #server is the server you want to copy from
 server="cronus.sargus.co.za"
 #sourcepath is the path where the content is on the server
-sourcepath="/root/Finished/*"
+sourcepath="/opt/Finished/*"
 #destpath is the path on the local server
 destpath="/media/other/"
 lock="/var/run/rsync.lock"
 
-echo "check to see if it's between 9pm and 6am"
-currenttime=$(date +%H:%M)
-if [[ "$currenttime" > "$start" ]] || [[ "$currenttime" < "$end" ]]; then
-    if ! [ -f $lock ]; then
-        touch $lock
-    else
-        echo "lock is in place, already running"
-        exit
-    fi
-    while true; do
-        # rsync --bwlimit=500 -avz --progress --timeout=60 --partial $user@$server:$sourcepath $destpath
-        rsync -avz --progress --timeout=60 --partial $user@$server:"$sourcepath" $destpath
+if ! [ -f $lock ]; then
+    touch $lock
+else
+    echo "lock is in place, already running"
+    exit
+fi
+while true; do
+    rsync -avz --progress --timeout=60 --partial $user@$server:"$sourcepath" $destpath
         check=$?
         if [ $check = "0" ]; then
             echo "rsync completed normally"
@@ -36,6 +30,3 @@ if [[ "$currenttime" > "$start" ]] || [[ "$currenttime" < "$end" ]]; then
         chmod -R 777 $destpath
         rm -f $lock
     done
-else
-    echo "not ready to rsync"
-fi
--- youtube.sh
+++ youtube.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -x 
+set -x
 folder=$1
 playlist=$2
 playlist="https://www.youtube.com/playlist?list=$playlist"
@@ -8,11 +8,16 @@ opt="-x --download-archive $destpath/archive.txt"
 destpath="/media/other/youtube-$folder"
 lock="/var/run/youtube.lock"
 
+#/*************  ✨ Codeium Command 🌟  *************/
 handle_error() {
+    echo "An error occurred on line $1" >&2
+    logger "An error occurred on line $1"
+    /bin/rm -f $lock
     echo "An error occurred on line $1"
-   /bin/rm -f $lock
+    /bin/rm -f $lock
     exit 1
 }
+#x/******  07d8d3c1-2ca7-45e2-bd0a-af4de4828599  *******/
 
 trap 'handle_error $LINENO' ERR
 if ! [ -f $lock ]; then
@@ -21,18 +26,17 @@ else
     echo "lock is in place, already running"
     exit
 fi
-while [ 1 ]
+while true
 do
-  mkdir -p $destpath
-  cd $destpath
-  /usr/bin/yt-dlp $opt $playlist
-  if [ "$?" = "0" ] ; then
-      echo "youtube-dl completed normally"
-  else
-      echo "youtube failure. Backing off and retrying..."
-              sleep 180
-  fi
-  done
-        /bin/chown -R nobody:nogroup $destpath
-        /bin/chmod -R 777 $destpath
+    mkdir -p "$destpath"
+    cd "$destpath" || exit
+     if /usr/bin/yt-dlp "$opt" "$playlist"; then
+        echo "youtube-dl completed normally"
+    else
+        echo "youtube failure. Backing off and retrying..."
+        sleep 180
+    fi
+    done
+        /bin/chown -R nobody:nogroup "$destpath"
+        /bin/chmod -R 777 "$destpath"
         /bin/rm -f $lock
