69cf359393a5cfa20e139ec9d8cae7bc0cc33d57
Angus Robinson <angus@fairhaven.za.net>
2025-11-17 06:52:10 +0200

    Adding clean-mac and youtbe

---
+++ clean-mac.sh
@@ -0,0 +1,7 @@
+sudo tmutil thinlocalsnapshots / 10000000000 4
+sudo rm -rf /Library/Caches/*
+sudo rm -rf ~/Library/Caches/*
+rm -rf ~/Library/Logs/*
+sudo rm -rf /private/var/log/*
+sudo rm /private/var/vm/sleepimage
+sudo rm /private/var/vm/swapfile*≈y
--- youtube.sh
+++ youtube.sh
@@ -1,42 +1,60 @@
 #!/usr/bin/env bash
 set -x
+
 folder=$1
 playlist=$2
+#playlist="https://www.youtube.com/playlist?list=PLvGjTP9F936rV8jisTJNgab_6YGI3sdq9"
 playlist="https://www.youtube.com/playlist?list=$playlist"
-opt="-x --download-archive $destpath/archive.txt"
 #destpath is the path on the local server
 destpath="/media/other/youtube-$folder"
-lock="/var/run/youtube.lock"
+lock="/var/run/youtube-$folder.lock"
+opt="-x --download-archive $destpath/archive.txt --cookies /root/.cookies.txt"
+cmd="/usr/bin/yt-dlp $opt $playlist"
+LOG_FILE="/var/log/youtube/$1-$(date +\%Y\%m\%d).log"
 
-#/*************  ✨ Codeium Command 🌟  *************/
-handle_error() {
-    echo "An error occurred on line $1" >&2
-    logger "An error occurred on line $1"
-    /bin/rm -f $lock
-    echo "An error occurred on line $1"
-    /bin/rm -f $lock
-    exit 1
+# Function to log messages with a timestamp
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
 }
-#x/******  07d8d3c1-2ca7-45e2-bd0a-af4de4828599  *******/
 
-trap 'handle_error $LINENO' ERR
-if ! [ -f $lock ]; then
-    /usr/bin/touch $lock
-else
-    echo "lock is in place, already running"
-    exit
-fi
-while true
-do
-    mkdir -p "$destpath"
-    cd "$destpath" || exit
-     if /usr/bin/yt-dlp "$opt" "$playlist"; then
-        echo "youtube-dl completed normally"
+    log_message "Script $1 started."
+    handle_error() {
+        log_message "An error occurred on line $1"
+        /bin/rm -f "$lock"
+        exit 1
+    }
+        log_message "Checking if lock is in place"
+    trap 'handle_error $LINENO' ERR
+    if ! [ -f "$lock" ]; then
+        /usr/bin/touch "$lock"
     else
-        echo "youtube failure. Backing off and retrying..."
+        log_message "lock is in place, already running"
+        exit
+    fi
+    while true
+    do
+
+        log_message "Checking if $destpath exists"
+    if ! [ -d "$destpath" ]; then
+        mkdir -p "$destpath"
+    else
+        log_message "$destpath exists"
+    fi
+        cd "$destpath" || exit
+        $cmd
+    if [ "$cmd" = "0" ] ; then
+        log_message "youtube-dl completed normally"
+    else
+        log_message "youtube failure. Backing off and retrying..."
         sleep 180
     fi
     done
+        log_message "Going to chown and chmod $destpath"
         /bin/chown -R nobody:nogroup "$destpath"
+        log_message "Chown done"
+        log_message "Starting chmod"
         /bin/chmod -R 777 "$destpath"
-        /bin/rm -f $lock
+        log_message "chmod done"
+        log_message "removing lock"
+        /bin/rm -f "$lock"
+        log_message "lock removed"
